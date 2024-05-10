2d6b30b1a1c8b2cc2b23618722e474dafdce5119
Ismael <ismael.calero@nter.es>
2024-05-10 12:48:53 +0200

    jwt service

---
+++ book-app-backend/src/main/java/com/ismael/bookappbackend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ismael.bookappbackend.security;
+
+import com.ismael.bookappbackend.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional // Cuando cargue el user, cargar los roles y autorizaciones con el usuario
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
