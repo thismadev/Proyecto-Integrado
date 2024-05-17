3cf3c681593d8bf84f13c7ee4f24f36a11117cc4
Ismael <ismael.calero@nter.es>
2024-05-17 11:52:56 +0200

    implementar controller y distintas llamadas

---
+++ book-app-backend/src/main/java/com/ismael/bookappbackend/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.ismael.bookappbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Ismael",
+                        email = "ismaelmindset@gmail.com",
+                        url = "https://www.linkedin.com/in/thismadev/"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Ismael",
+                version = "1.0",
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "bearerAuth"
+        )
+)
+
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
